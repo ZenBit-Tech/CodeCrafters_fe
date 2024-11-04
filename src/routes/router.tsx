da8a292d50a970d3ed1640c218efdde36c3cbbf0
oyakovytskyi <oyakovytskyi.dev@gmail.com>
2024-11-04 14:04:19 +0200

    feat: add new route for example and implement logic to send email and token to backend

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,7 +1,11 @@
 import { createBrowserRouter } from 'react-router-dom';
 
 import SignInPage from '@/pages/SignIn';
+import OrdersPage from '@/pages/Orders';
 
-const router = createBrowserRouter([{ index: true, element: <SignInPage /> }]);
+const router = createBrowserRouter([
+  { index: true, element: <SignInPage /> },
+  { path: 'orders', element: <OrdersPage /> },
+]);
 
 export default router;
