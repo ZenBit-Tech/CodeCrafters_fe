fd703cbe443c5bb9449c3783d4caca5dfed4c56e
iraboldyrieva <iraboldyrieva@gmail.com>
2024-11-06 11:11:05 +0200

    Fixed new branch withlayout component

---
+++ src/components/Layout/RootLayout/index.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { Outlet } from 'react-router-dom';
+
+import { assets } from '@/assets/assets';
+import ListIcon from '@mui/icons-material/List';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import LuggageIcon from '@mui/icons-material/Luggage';
+import SettingsIcon from '@mui/icons-material/Settings';
+import { AppProvider } from '@toolpad/core/react-router-dom';
+
+import type { Navigation } from '@toolpad/core';
+const RootLayout: React.FC = () => {
+  const { t } = useTranslation();
+
+  const NAVIGATION: Navigation = React.useMemo(
+    () => [
+      {
+        segment: '/orders',
+        title: t('navigation.orders'),
+        icon: <LuggageIcon />,
+      },
+      {
+        segment: '/routes',
+        title: t('navigation.routes'),
+        icon: <LocationOnIcon />,
+      },
+      {
+        segment: '/settings',
+        title: t('navigation.settings'),
+        icon: <SettingsIcon />,
+      },
+      {
+        segment: '/company-list',
+        title: t('navigation.companyList'),
+        icon: <ListIcon />,
+      },
+    ],
+    [t]
+  );
+
+  const BRANDING = {
+    logo: (
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <img
+          src={assets.logoIcon}
+          alt="First Logo"
+          style={{ height: '40px' }}
+        />
+        <img
+          src={assets.logoText}
+          alt="Second Logo"
+          style={{ height: '40px', marginLeft: '8px' }}
+        />
+      </div>
+    ),
+    title: '',
+  };
+
+  return (
+    <AppProvider navigation={NAVIGATION} branding={BRANDING}>
+      <Outlet />
+    </AppProvider>
+  );
+};
+
+export default RootLayout;
--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,7 +1,22 @@
 import { createBrowserRouter } from 'react-router-dom';
 
-import SignInPage from '@/pages/SignIn';
+import DashboardLayout from '@/components/Layout/DashboardLayout';
+import RootLayout from '@/components/Layout/RootLayout';
+import DashboardPage from '@/pages/DashboardPage';
+import ExamplePage from '@/pages/ExamplePage';
 
-const router = createBrowserRouter([{ index: true, element: <SignInPage /> }]);
+const router = createBrowserRouter([
+  { index: true, element: <ExamplePage /> },
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      {
+        element: <DashboardLayout />,
+        children: [{ path: '/', element: <DashboardPage /> }],
+      },
+    ],
+  },
+]);
 
 export default router;
