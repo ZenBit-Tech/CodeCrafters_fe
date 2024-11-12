cb18bbfaba41a07ce362462120c3b924145365d8
oyakovytskyi <oyakovytskyi.dev@gmail.com>
2024-11-12 20:04:08 +0200

    chore(router): configure router to support recent changes

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,35 +1,58 @@
-import { createBrowserRouter } from 'react-router-dom';
-
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import DashboardLayout from '@/components/Layout/DashboardLayout';
 import RootLayout from '@/components/Layout/RootLayout';
 import DashboardPage from '@/pages/DashboardPage';
-import ExamplePage from '@/pages/ExamplePage';
 import SignInPage from '@/pages/SignIn';
-import OrdersPage from '@/pages/Orders';
 import ProtectedRoute from '@/pages/components/ProtectedRoute';
+import NotFoundPage from '@/pages/NotFound';
+import { RootState } from '@/store/store';
 
-const router = createBrowserRouter([
-  { index: true, element: <ExamplePage /> },
-  {
-    path: '/',
-    element: <RootLayout />,
-    children: [
-      {
-        element: <DashboardLayout />,
-        children: [{ path: '/', element: <DashboardPage /> }],
-      },
-    ],
-  },
-  {
-    path: '/signin',
-    element: (
-      <ProtectedRoute>
-        <SignInPage />
-      </ProtectedRoute>
-    ),
-  },
-  { path: '/orders', element: <OrdersPage /> },
-]);
+const AppRouter: React.FC = () => {
+  const isAuthenticated = useSelector(
+    (state: RootState) => state.auth.isAuthenticated
+  );
 
-export default router;
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: isAuthenticated ? (
+        <RootLayout />
+      ) : (
+        <ProtectedRoute isAllowed={!isAuthenticated}>
+          <SignInPage />
+        </ProtectedRoute>
+      ),
+      children: [
+        {
+          path: 'dashboard',
+          element: (
+            <ProtectedRoute isAllowed={isAuthenticated}>
+              <DashboardLayout />
+            </ProtectedRoute>
+          ),
+          children: [
+            {
+              index: true,
+              element: (
+                <ProtectedRoute isAllowed={isAuthenticated}>
+                  <DashboardPage />
+                </ProtectedRoute>
+              ),
+            },
+          ],
+        },
+        {
+          path: '*',
+          element: <NotFoundPage />,
+        },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
+};
+
+export default AppRouter;
