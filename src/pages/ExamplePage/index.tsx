139d70d5d988eb49c7fb45d81a7590e6f0a511d5
Parle648 <sashahoumi@gmail.com>
2024-11-15 15:32:41 +0200

    + Create: company & admin crud components template

--- src/pages/ExamplePage/index.tsx
+++ src/pages/ExamplePage/index.tsx
@@ -5,6 +5,11 @@ import { useTranslation } from 'react-i18next';
 import ExampleComponent from '@/components/ExampleComponent';
 import useExamplePageStyles from '@/pages/ExamplePage/styles';
 import useExamplePageLogic from '@/pages/ExamplePage/useExamplePage';
+import AddNewAdmin from './AddNewAdmin/AddNewAdmin';
+import UpdateAdmin from './UpdateAdmin/UpdateAdmin';
+import DeleteAdmin from './DeleteAdmin/DeleteAdmin';
+import AddNewCompany from './AddCompany/AddNewCompany';
+import UpdateCompany from './UpdateCompany/UpdateCompany';
 
 const ExamplePage = (): React.ReactElement => {
   const { t } = useTranslation();
@@ -15,6 +20,12 @@ const ExamplePage = (): React.ReactElement => {
     <div className={classes.container}>
       <h1>{t(title)}</h1>
       <ExampleComponent />
+      <AddNewAdmin companyId={1} />
+      <UpdateAdmin userId={2} />
+      <DeleteAdmin adminId={2} />
+      <br />
+      <AddNewCompany />
+      <UpdateCompany companyId={1} />
     </div>
   );
 };
