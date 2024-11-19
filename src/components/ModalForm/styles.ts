4be5571aca5a8cc9e740d0d8d07fc551eb7b780b
oyakovytskyi <oyakovytskyi.dev@gmail.com>
2024-11-19 18:43:39 +0200

    fix: change background for modal blur

--- src/components/ModalForm/styles.ts
+++ src/components/ModalForm/styles.ts
@@ -1,10 +1,12 @@
 import styled from '@emotion/styled';
 import { SxProps, Theme } from '@mui/material';
 
+import { COLORS } from '@/constants/colors';
+
 export const formWrapper: SxProps<Theme> = {
   width: '100vw',
   height: '100vh',
-  backgroundColor: (theme) => theme.palette.background.default,
+  backgroundColor: COLORS.text.light,
   position: 'fixed',
   top: '0',
   left: '0',
