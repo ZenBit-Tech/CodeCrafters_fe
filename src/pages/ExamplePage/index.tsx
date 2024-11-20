7fc83033bb8da05d677d70e5186db2b3fb2bb00d
Parle648 <sashahoumi@gmail.com>
2024-11-20 13:47:22 +0200

    * Fix: PR issues

--- src/pages/ExamplePage/index.tsx
+++ src/pages/ExamplePage/index.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 
 import { useTranslation } from 'react-i18next';
 
-import ExampleComponent from '@/components/ExampleComponent';
-// import useExamplePageStyles from '@/pages/ExamplePage/styles';
 import useExamplePageLogic from '@/pages/ExamplePage/useExamplePage';
 import AddNewAdmin from './AddNewAdmin/AddNewAdmin';
 import UpdateAdmin from './UpdateAdmin/UpdateAdmin';
@@ -18,7 +16,6 @@ const ExamplePage = (): React.ReactElement => {
   return (
     <div>
       <h1>{t(title)}</h1>
-      <ExampleComponent />
       <AddNewAdmin companyId={1} />
       <UpdateAdmin userId={2} />
       <DeleteAdmin adminId={2} />
