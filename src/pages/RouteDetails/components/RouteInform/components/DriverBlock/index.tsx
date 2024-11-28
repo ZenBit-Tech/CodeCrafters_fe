20de2e70c6b002768c4200c5afb78f4731069e65
Parle648 <sashahoumi@gmail.com>
2024-11-28 17:49:18 +0200

    + Add: t function

--- src/pages/RouteDetails/components/RouteInform/components/DriverBlock/index.tsx
+++ src/pages/RouteDetails/components/RouteInform/components/DriverBlock/index.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react';
 import { Box, Typography } from '@mui/material';
+import { t } from 'i18next';
 
 import DriverAvatar from '@/components/DriverAvatar';
 import {
@@ -13,10 +14,10 @@ const DriverBlock: FC = () => {
     <Box sx={driverBlockContainerStyles}>
       <DriverAvatar firstName={'John'} lastName={'Doe'} />
       <Box sx={routePropsBlockStyles}>
-        <Typography>John Doe</Typography>
+        <Typography>{t('John Doe')}</Typography>
         <Box sx={routePropsRowStyles}>
           <Typography>09:00 - 18:00</Typography>
-          <Typography>5 stops</Typography>
+          <Typography>{t('5 stops')}</Typography>
           <Typography>900 km</Typography>
         </Box>
       </Box>
