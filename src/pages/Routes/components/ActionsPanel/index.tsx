8b5e241ab0ab43e328dc5f12ebf6eb46dd415fe6
oyakovytskyi <oyakovytskyi.dev@gmail.com>
2024-11-29 10:06:14 +0200

    feat: add driver name search functionality

--- src/pages/Routes/components/ActionsPanel/index.tsx
+++ src/pages/Routes/components/ActionsPanel/index.tsx
@@ -1,27 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import SearchIcon from '@mui/icons-material/Search';
+import { InputAdornment } from '@mui/material';
 
 import {
   ActionsContainer,
   SearchContainer,
   ActionButtonsContainer,
   RopeIcon,
+  StyledTextField,
 } from '@/pages/Routes/styles';
 import CalendarRange from '@/components/CalendarRange';
-import TextInput from '@/components/TextInput';
 import ropeIcon from '@/assets/icons/jump-rope.svg';
 import Button from '@/components/Button';
 
-const ActionsPanel: React.FC = () => {
+const ActionsPanel: React.FC<{
+  onDateChange: (start: string, end: string) => void;
+  onSearchChange: (searchQuery: string) => void;
+}> = ({ onDateChange, onSearchChange }) => {
+  const [searchQuery, setSearchQuery] = useState('');
   const { t } = useTranslation();
 
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(e.target.value);
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      onSearchChange(searchQuery);
+    }
+  };
+
+  const handleSearchClick = () => {
+    onSearchChange(searchQuery);
+  };
   return (
     <ActionsContainer>
       <SearchContainer>
-        <TextInput label={t('routesPage.search')} />
+        <StyledTextField
+          label={t('routesPage.search')}
+          value={searchQuery}
+          onChange={handleInputChange}
+          onKeyDown={handleKeyDown}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end" onClick={handleSearchClick}>
+                <SearchIcon />
+              </InputAdornment>
+            ),
+          }}
+        />
       </SearchContainer>
       <ActionButtonsContainer>
-        <CalendarRange />
+        <CalendarRange onDateChange={onDateChange} />
         <Button
           variant="lined"
           label={t('routesPage.mapView')}
