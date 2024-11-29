55569795df2a4c2245e5418dff46d0cb9b2b8fec
oyakovytskyi <oyakovytskyi.dev@gmail.com>
2024-11-29 10:03:38 +0200

    feat(api): create file for calling route endpoints

---
+++ src/api/routesActions.ts
@@ -0,0 +1,42 @@
+import { toast } from 'react-toastify';
+import i18n from '@/utils/i18n';
+import { AxiosError } from 'axios';
+
+import axiosInstance from '@/utils/axiosInstance';
+import { BackendRoute } from '@/interfaces/Routes';
+
+export const getRoutesByDateRange = async (
+  startDate: string,
+  endDate: string,
+  sortField: string,
+  sortDirection: 'asc' | 'desc',
+  searchQuery?: string,
+  drivers?: string[],
+  stopsCount?: number[],
+  statuses?: string[]
+): Promise<BackendRoute[]> => {
+  try {
+    const response = await axiosInstance.get('/routes/by-dates', {
+      params: {
+        startDate,
+        endDate,
+        sortField,
+        sortDirection,
+        searchQuery,
+        drivers,
+        stopsCount,
+        statuses,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    if (error instanceof AxiosError && error.response) {
+      const errorMessage =
+        error.response.data.message || i18n.t('routesApi.unexpected_error');
+      toast.error(i18n.t(errorMessage));
+    } else {
+      toast.error(i18n.t('routesApi.fetch_failed'));
+    }
+    throw new Error(i18n.t('routesApi.unexpected_error'));
+  }
+};
