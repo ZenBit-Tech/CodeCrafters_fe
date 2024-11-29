be595a76abef7746cf4a132aa93d69cf4cc8b6ba
oyakovytskyi <oyakovytskyi.dev@gmail.com>
2024-11-29 10:03:49 +0200

    feat(calendar): pass selected date range from calendar

--- src/components/CalendarRange/index.tsx
+++ src/components/CalendarRange/index.tsx
@@ -5,11 +5,23 @@ import { DateRangePicker } from '@mui/x-date-pickers-pro/DateRangePicker';
 import { SingleInputDateRangeField } from '@mui/x-date-pickers-pro/SingleInputDateRangeField';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { Dayjs } from 'dayjs';
 
-const CalendarRange: React.FC = () => {
+const CalendarRange: React.FC<{
+  onDateChange: (start: string, end: string) => void;
+}> = ({ onDateChange }) => {
+  const handleDateChange = (range: [Dayjs | null, Dayjs | null]) => {
+    if (range[0] && range[1]) {
+      onDateChange(
+        range[0].format('YYYY-MM-DD'),
+        range[1].format('YYYY-MM-DD')
+      );
+    }
+  };
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <DateRangePicker
+        onChange={(newValue) => handleDateChange(newValue)}
         slots={{ field: SingleInputDateRangeField }}
         slotProps={{
           textField: {
