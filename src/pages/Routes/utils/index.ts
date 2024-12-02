efc6d292fda1d1415f2381c25061ab68da66faa4
oyakovytskyi <oyakovytskyi.dev@gmail.com>
2024-12-02 12:36:08 +0200

    refactor(utils): move helper functions to utils

---
+++ src/pages/Routes/utils/index.ts
@@ -0,0 +1,33 @@
+export const formatDate = (dateString: string): string => {
+  const date = new Date(dateString);
+  return new Intl.DateTimeFormat('en-GB', {
+    day: '2-digit',
+    month: 'short',
+    year: 'numeric',
+  }).format(date);
+};
+
+export const calculateRouteTime = (
+  startDate: string,
+  endDate: string
+): string => {
+  const start = new Date(startDate);
+  const end = new Date(endDate);
+
+  const formatTime = (date: Date): string => {
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const formattedMinutes = minutes < 10 ? `0${minutes}` : `${minutes}`;
+    return `${hours}.${formattedMinutes}`;
+  };
+
+  const startTime = formatTime(start);
+  const endTime = formatTime(end);
+
+  return `${startTime} - ${endTime}`;
+};
+
+export const processFullName = (fullName: string) => {
+  const [firstName, lastName] = fullName.split(' ');
+  return { firstName, lastName };
+};
