1aa322179c8d0009a057e063be15aa6a2285424a
oyakovytskyi <oyakovytskyi.dev@gmail.com>
2024-12-03 14:41:22 +0200

    feat(constants): add constants for driver data management

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -3,5 +3,6 @@ export const LOGO = 'http://logo-sample';
 export const ITEMS_PER_PAGE_OPTIONS = [10, 20, 30];
 
 export const ADMINROLE = 'admin';
+export const DRIVERROLE = 'driver';
 
 export const START_ROUTE_POINT = 'New York';
