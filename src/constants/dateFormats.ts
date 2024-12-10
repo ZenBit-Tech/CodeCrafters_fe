82eccb94ddb976c4bbad957f5fba337058c6e757
oyakovytskyi <oyakovytskyi.dev@gmail.com>
2024-12-10 05:36:58 +0200

    feat(constants): new constants for dates

--- src/constants/dateFormats.ts
+++ src/constants/dateFormats.ts
@@ -2,4 +2,6 @@ export const DAY_MONTH_WEEKDAY = 'DD MMMM, dddd';
 
 export const FULL_TIME = 'HH:mm';
 
-export const DATE_FORMAT = 'YYYY-MM-DD';
+export const DATE_FORMAT = 'MM.dd.yyyy';
+
+export const MONTH_YEAR = 'MMMM yyyy';
